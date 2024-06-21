295d40c16aa5405d267a2b6f918f5d61b70c5dfa
Julia Bogdanova <bogdanovayn@vdcom.ru>
2024-06-21 15:56:51 +0400

    Add frontend

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -109,9 +109,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL =  '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = ''
