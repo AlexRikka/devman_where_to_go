77d03ce9172567672fd3d386156a42aa19c4702d
AlexRikka <rikka.gold@mail.ru>
2024-06-29 02:43:28 +0400

    Get places from database to front

--- places/views.py
+++ places/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
+from places.models import Place
 
 
 def index(request):
-    context = {}
+    places = []
+    for place in Place.objects.all():
+        places.append({
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.lon, place.lat]
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": place.id,
+                "detailsUrl": "./static/places/moscow_legends.json"
+            }
+        })
+
+    places_geojson = {
+        "type": "FeatureCollection",
+        "features": places
+    }
+    context = {'places': places_geojson}
     return render(request, 'index.html', context)
